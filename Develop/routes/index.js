ef95d2db6ad4c27b5f2d49bc77c6e219ecc1849c
Sam Kilpatrick <samkilpatrick903@gmail.com>
2021-11-24 14:37:24 -0600

    routes working

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,18 +1,68 @@
 const router = require("express").Router();
 const { Workout } = require("../models");
+const path = require("path");
+const mongoose = require("mongoose");
 
-router.get("/api/workouts", async(req, res) => {
-  try  {
-  let docs = await Workout.aggregate([
-     {
-       $addFields: {totalDuration: {$sum: "$exercises.duration"}}
-     }
-   ])
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+router.get("/api/workouts", async (req, res) => {
+  try {
+    let docs = await Workout.aggregate([
+      {
+        $addFields: { totalDuration: { $sum: "$exercises.duration" } },
+      },
+    ]);
     console.log(docs);
     res.json(docs);
-  }
-  catch(err) {
-    console.log(err)
+  } catch (err) {
+    console.log(err);
   }
 });
+
+router.put("/api/workouts/:id", async (req, res) => {
+  try {
+    const put = await Workout.updateOne(
+      { _id: mongoose.Types.ObjectId(req.params.id) },
+      { $push: { exercises: req.body } },
+      { new: true }
+    );
+    res.json(put);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post("/api/workouts", async (req,res) => {
+    const newWorkout = req.body;
+    const exercise = await Workout.create(newWorkout);
+    console.log(exercise);
+    res.json(exercise);
+    
+  });
+  
+  router.get("/api/workouts/range", async (req,res) => {
+    try{
+    const past = await Workout.aggregate([
+      {
+        $sort: {_id: -1}
+      },
+      {
+        $limit: 7
+      },
+      {
+        $addFields: {totalDuration: {$sum: "$exercises.duration"}}
+      }
+    ])
+    res.json(past)
+  } catch(err) {
+    res.json(err)};
+  });
+
+
 module.exports = router;
