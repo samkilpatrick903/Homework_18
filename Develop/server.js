fd362e88f8cba6264ab1f93661e94479a6cc1ecf
Sam Kilpatrick <samkilpatrick903@gmail.com>
2021-11-28 14:55:00 -0600

    updated index.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,7 @@
-
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const routes = require('./routes');
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,3 +13,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Homework_18
-Workout Tracker
+# NoSQL Workout Tracker
+
+## Description
+A full-stack application that creates and tracks daily workouts. The application creates a Mongo database with a Mongo schema. Built using Node.js and bootstrap.
+
+## Screenshot
+
+## Deployed Link
